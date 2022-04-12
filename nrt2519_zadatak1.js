d609b3cdf5352e3e3c1a9f0a2693c8863e7a6070
antonije <antonijenrt2519@gs.viser.edu.rs>
2022-04-12 21:35:34 +0200

    dodavanje svega

---
+++ nrt2519_zadatak1.js
@@ -0,0 +1,16 @@
+const math=require("mathjs")
+
+let a=5
+let b=10
+let ugao=30
+
+function sabiranje(a,b){
+	return a+b
+}
+function oduzimanje(a,b){
+	return a-b
+}
+function stepenovanje(a,b){
+	return math.pow(a,b)
+}
+
